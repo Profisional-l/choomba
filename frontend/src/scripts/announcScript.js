19370bf32d24347b989b6decb0959cb63c764507
Jake Sooly <96531067+Profisional-l@users.noreply.github.com>
2024-09-29 16:26:16 +0300

    split work try

--- frontend/src/scripts/announcScript.js
+++ frontend/src/scripts/announcScript.js
@@ -7,17 +7,21 @@ export const AnnouncScript = () => {
     const [description, setDescription] = useState('');
     const [deleteId, setDeleteId] = useState('');
 
+    // Определяем базовый URL в зависимости от среды
+    const isLocal = window.location.hostname === 'localhost';
+    const API_URL = isLocal ? 'http://localhost:5000' : 'api';
+
     useEffect(() => {
         fetchAnnouncements();
     }, []);
 
     const fetchAnnouncements = async () => {
-        const response = await axios.get('api/announcements');
+        const response = await axios.get(`${API_URL}/announcements`);
         setAnnouncements(response.data);
     };
 
     const createAnnouncement = async () => {
-        await axios.post('api/announcements', { title, description });
+        await axios.post(`${API_URL}/announcements`, { title, description });
         fetchAnnouncements();
         setTitle('');
         setDescription('');
@@ -28,7 +32,7 @@ export const AnnouncScript = () => {
             alert('Пожалуйста, введите id объявления для удаления.');
             return;
         }
-        await axios.delete(`api/announcements/${deleteId}`);
+        await axios.delete(`${API_URL}/announcements/${deleteId}`);
         fetchAnnouncements();
         setDeleteId('');
     };
