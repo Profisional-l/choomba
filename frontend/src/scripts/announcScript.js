d7a55f577db295a19ead4472841ebfb47efb0f46
Jake Sooly <96531067+Profisional-l@users.noreply.github.com>
2024-10-01 18:05:48 +0300

    wer

--- frontend/src/scripts/announcScript.js
+++ frontend/src/scripts/announcScript.js
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { userName } from './takeTGinfo';
 
+
 export const AnnouncScript = () => {
     const [announcements, setAnnouncements] = useState([]);
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
     const [deleteId, setDeleteId] = useState('');
 
+    // Определяем базовый URL в зависимости от среды
     const isLocal = window.location.hostname === 'localhost';
     const API_URL = isLocal ? 'http://localhost:5000' : '/api';
 
@@ -16,27 +18,15 @@ export const AnnouncScript = () => {
     }, []);
 
     const fetchAnnouncements = async () => {
-        try {
-            const response = await axios.get(`${API_URL}/announcements`);
-            setAnnouncements(response.data);
-        } catch (error) {
-            console.error('Ошибка при получении объявлений:', error);
-        }
+        const response = await axios.get(`${API_URL}/announcements`);
+        setAnnouncements(response.data);
     };
 
     const createAnnouncement = async () => {
-        if (!description) {
-            alert('Пожалуйста, заполните все поля.');
-            return;
-        }
-        try {
-            await axios.post(`${API_URL}/announcements`, { title, description });
-            setTitle(''); // Сбрасываем title после успешного создания
-            setDescription(''); // Сбрасываем description после успешного создания
-            fetchAnnouncements();
-        } catch (error) {
-            console.error('Ошибка при создании объявления:', error);
-        }
+        await axios.post(`${API_URL}/announcements`, { title, description });
+        fetchAnnouncements();
+        setTitle(userName);
+        setDescription('');
     };
 
     const deleteAnnouncement = async () => {
@@ -44,13 +34,9 @@ export const AnnouncScript = () => {
             alert('Пожалуйста, введите id объявления для удаления.');
             return;
         }
-        try {
-            await axios.delete(`${API_URL}/announcements/${deleteId}`);
-            fetchAnnouncements();
-            setDeleteId('');
-        } catch (error) {
-            console.error('Ошибка при удалении объявления:', error);
-        }
+        await axios.delete(`${API_URL}/announcements/${deleteId}`);
+        fetchAnnouncements();
+        setDeleteId('');
     };
 
     return { announcements, createAnnouncement, deleteAnnouncement, setTitle, setDescription, setDeleteId };
